6db24504fe0a3a0bef40da1035797b0424e1d872
Ryan Seit <ryan@ryanseit.com>
2020-03-11 22:52:03 -0400

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,14 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+
+    # def initialize(args)
+    #     @title = args[:title]
+    #     @content = args[:content]
+    # end
+    
+    def self.search(query)
+        where("name like ? ", "%#{ query }%")
+    end
+
 end
